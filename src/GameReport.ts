c72534e8bfe24bfba6d4f118dac88178986ff3d4
Indiara C. Menegat <indicm@gmail.com>
2024-05-28 22:21:01 -0300

    eslint

--- src/GameReport.ts
+++ src/GameReport.ts
@@ -4,11 +4,11 @@ import { IReport } from './model/IReport';
 
 export class GameReport {
   private readonly games: Game[];
-  private readonly report: Report[]
+  private readonly report: Report[];
 
   constructor(games: Game[]) {
     this.games = games;
-    this.report = this.games.map((game) => new Report(game))
+    this.report = this.games.map((game) => new Report(game));
   }
 
   toJSON(): IReport[] {
@@ -18,5 +18,4 @@ export class GameReport {
   // toPlainText() {
 
   // }
-
 }
