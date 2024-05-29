0b1e76960f9258f7e89843c5084093491573fd96
Indiara C. Menegat <indicm@gmail.com>
2024-05-29 19:56:48 -0300

    cli happy path (valid file intput + select output)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,32 @@
+import { input, select } from '@inquirer/prompts';
 import { LogParser } from './controllers/LogParser';
 import { GameReport } from './controllers/GameReport';
+import { existsSync } from 'fs';
 
-const logPath = 'data/qgames.log';
+(async () => {
+  const filePath = await input({
+    message: 'Add file path to parse',
+    validate: (value) => {
+      if (existsSync(value)) return true;
+      return `Caminho do arquivo inválido, o arquivo não existe`;
+    },
+  });
 
-const parser = new LogParser(logPath);
-const reporter = new GameReport(parser.getGames());
-// console.log(JSON.stringify(reporter.toJSON()));
-console.log(reporter.toPlainText());
+  const parser = new LogParser(filePath);
+  const reporter = new GameReport(parser.getGames());
+
+  const outputFormat = await select({
+    message: 'Would you link the output in which format?',
+    choices: [
+      {
+        value: 'JSON',
+      },
+      {
+        value: 'Text',
+      },
+    ],
+  });
+
+  if (outputFormat === 'JSON') console.log(reporter.toJSON());
+  if (outputFormat === 'Text') console.log(reporter.toPlainText());
+})();
